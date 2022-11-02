4a4e281a97e08fd8a75cd5ddea3f2837ada8fd42
Chamber6821 <vania.osinin@yandex.ru>
2022-11-03 00:00:12 +0300

    feat: implement the main logic and add example.ini

--- main.c
+++ main.c
@@ -1,7 +1,27 @@
+#include "c-vector/vec.h"
+#include "my-db/Database.h"
+#include "my-db/handlers.h"
+#include "my-db/ini/Config.h"
+#include "my-db/ini/reader.h"
+#include "my-db/ini/writer.h"
+#include "my-db/request.h"
+
 int main() {
-    // 1. load db table
-    // 2. load all requests
-    // 3. process 1 request
-    // 4. upload 1 response
-    // 5. repeat from 3
+    Database *db = createEmptyDatabase();
+
+    // все популярные ОСи поддерживают перенаправление потока ввода из файла
+    Config *allRequests = readConfigFile(stdin);
+
+    for (int i = 0; i < vector_size(allRequests->sections); i++) {
+        Request *request = tryBuildRequest(allRequests->sections[i]);
+        Section *result = handleRequest(db, request);
+
+        // все популярные ОСи поддерживают перенаправление потока вывода в файл
+        dumpSectionToFile(stdout, result);
+
+        freeSection(result);
+        freeRequest(request);
+    }
+
+    freeDatabase(db);
 }
